9204e19bb2ff806598f6e0aaf0733a92aa3608d0
Graham Ashton <graham@effectif.com>
2026-07-10 09:10:42 +0100

    Implement happy path for creating invoices
    There are a couple of things to note here:
    
    - The Zod validation code will throw an error if values are missing, or
      of the wrong type. We're not handling that yet.
    
    - Next caches quite aggressively by default, to reduce the load on the
      server. `revalidatePath()` is responsible for ensuring that both
      client and server-side caches are invalidated on the next visit. Note
      that the path can either be the filesystem path (e.g. a dynamic route,
      such as "/foo/[slug]/bar") or the literal route for a specific
      request.
    
    See https://nextjs.org/learn/dashboard-app/mutating-data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,36 @@
 'use server';
 
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+import { z } from "zod";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(form: FormData) {
-  const attributes = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: form.get("customerId"),
     amount: form.get("amount"),
     status: form.get("status"),
-  }
-  console.log(attributes);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
